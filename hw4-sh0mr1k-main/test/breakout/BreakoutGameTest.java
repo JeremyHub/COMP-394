daad0429db8612494412d0f9f068a9db8358f3bd
JeremyHub <jeremyjhub@gmail.com>
2023-02-06 12:58:56 -0600

    Update BreakoutGameTest.java

--- hw4-sh0mr1k-main/test/breakout/BreakoutGameTest.java
+++ hw4-sh0mr1k-main/test/breakout/BreakoutGameTest.java
@@ -1,17 +1,24 @@
 package breakout;
 
+import edu.macalester.graphics.Point;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BreakoutGameTest {
 
     @Test
-    void main() {
-    }
-
-    @Test
-    void moveBall() {
+    @ParameterizedTest
+    @ValueSource(ints={0,10,50,100,150,200,300,350,360})
+    void moveBall(int angle) {
+        BreakoutGame game = new BreakoutGame();
+        BreakoutBall ball = new BreakoutBall(0,0,angle,100,100);
+        Point prev_point = ball.getPosition();
+        game.moveBall(ball);
+        assertNotEquals(prev_point.getX(), ball.getPosition().getX());
+        assertNotEquals(prev_point.getY(), ball.getPosition().getY());
     }
 
     @Test
